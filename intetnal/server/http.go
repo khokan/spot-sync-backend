cd9a8f584b0d901eb6a0d3b568b9928d9d860172
khokan <khokon@gmail.com>
2026-06-26 12:01:37 +0600

    env vairable and db connected

---
+++ intetnal/server/http.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"spotsync/intetnal/config"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+	"gorm.io/gorm"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
+func Start(db *gorm.DB, cfg *config.Config) {
+	// db.AutoMigrate(&user.User{}, &event.Event{}, &booking.Booking{})
+
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+	e.Use(middleware.RequestLogger())
+
+	e.GET("/health", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "spot-sync running")
+	})
+
+	port := fmt.Sprintf(":%s", cfg.Port)
+	if err := e.Start(port); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
